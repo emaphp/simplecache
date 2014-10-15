b09ecbc742d4e085f5014dbb1fa00326991bd0d6
emaphp <emmanuel.antico@gmail.com>
2014-10-15 20:27:25 -0300

    installing pecl

--- tests/apcu-install.sh
+++ tests/apcu-install.sh
@@ -4,6 +4,7 @@ APCU=4.0.2
 
 if [ "$TRAVIS_PHP_VERSION" == "5.4" ]
 then
+    sudo apt-get install pecl
     sudo pecl install apcu-beta
     # sudo apt-get install autoconf
     # wget http://pecl.php.net/get/apcu-$APCU.tgz
