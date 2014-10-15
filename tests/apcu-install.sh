9843b09900c70ab98b839d1bbebc82c163a95c69
emaphp <emmanuel.antico@gmail.com>
2014-10-15 20:39:18 -0300

    Remove additional commands

--- tests/apcu-install.sh
+++ tests/apcu-install.sh
@@ -4,11 +4,9 @@ APCU=4.0.2
 
 if [ "$TRAVIS_PHP_VERSION" == "5.4" ]
 then
-    sudo apt-get install pecl
-    sudo pecl install apcu-beta
-    # sudo apt-get install autoconf
-    # wget http://pecl.php.net/get/apcu-$APCU.tgz
-    # tar zxvf apcu-$APCU.tgz
-    # cd "apcu-${APCU}"
-    # phpize && ./configure && make install && echo "Installed ext/apcu-${APCU}"
+    sudo apt-get install autoconf
+    wget http://pecl.php.net/get/apcu-$APCU.tgz
+    tar zxvf apcu-$APCU.tgz
+    cd "apcu-${APCU}"
+    phpize && ./configure && make install && echo "Installed ext/apcu-${APCU}"
 fi
